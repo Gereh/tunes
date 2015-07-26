285b77a533e3d50886f066e87f7260806912f0b6
Gereh <ansari.robo@gmail.com>
2015-07-26 14:44:00 +0430

    added description&bio&profileImg field to Artist model

--- music/models.py
+++ music/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Artist(models.Model):
-    name = models.CharField()
-
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    bio = models.TextField()
+    profileImg = models.ImageField(upload_to="media/artist/profileImg")
 
 class Track(models.Model):
     name = models.CharField()
