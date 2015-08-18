35a2d04922a1b7baadba131e76850cffa08c1397
Mohammad Saeed Ansari <ansari.robo@gmail.com>
2015-08-18 15:16:30 +0430

    fix home page and added advertise in model and in home page

--- music/views.py
+++ music/views.py
@@ -6,6 +6,8 @@ def home(request):
     slider = Album.objects.all().order_by('publishDate')[:5]
     most_visited = Album.objects.all().order_by('visits')[:12]
     most_downloaded = Album.objects.all().order_by('downloads')[:12]
+    advertises = Advertise.objects.filter(position="home").order_by('createdAt')[:1]
     return render(request, 'home.html', {'slider': slider,
                                          'most_visited': most_visited,
-                                         'most_downloaded': most_downloaded})
+                                         'most_downloaded': most_downloaded,
+                                         'advertises': advertises})
