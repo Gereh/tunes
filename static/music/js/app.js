e2e933afd185826a14c1a09250397248eef5f24a
Mohammad Saeed Ansari <ansari.robo@gmail.com>
2015-08-18 17:09:28 +0430

    tst

---
+++ static/music/js/app.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+		var sliderBottom1 = new MasterSlider();
+		sliderBottom1.setup('ms-services-1' , {
+			loop:true,
+			width:300,
+			height:300,
+			speed:20,
+			view:'wave',
+			preload:0,
+			space:0,
+			wheel:true
+		});
+		sliderBottom1.control('arrows');
+		sliderBottom1.control('slideinfo',{insertTo:'#staff-info-services-1'});
+
+
