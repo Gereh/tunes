7c76e75d8153946e85f409dd619de4dac05979b1
syag2015 <s.a.ghoreishi1995@gmail.com>
2015-08-23 16:06:48 +0430

    form validation

--- music/forms.py
+++ music/forms.py
@@ -12,28 +12,49 @@ class Login(forms.Form):
     password = forms.CharField(label="password", max_length=20, widget=forms.PasswordInput)
     captcha = CaptchaField()
 
+    username.widget.attrs['required']= 'required'
     username.widget.attrs['placeholder'] = 'نام کاربری'
+
+    password.widget.attrs['required']= 'required'
     password.widget.attrs['placeholder'] = 'رمز عبور'
+
+    captcha.widget.attrs['required']= 'required'
     captcha.widget.attrs['placeholder'] = 'کد امنیتی'
+
 
 
 class Register(ModelForm):
     password2 = forms.CharField(max_length=25)
     captcha = CaptchaField(error_messages={'invalid':'کد امنیتی اشتباه می باشد'})
+
     password2.widget.attrs['required']= 'required'
-    captcha.widget.attrs['required']='کد امنیتی را وارد کنید'
+    password2.widget.attrs['placeholder']= 'پسورد'
+
+    captcha.widget.attrs['required']='required'
+    captcha.widget.attrs['placeholder']='کد امنیتی'
+
     class Meta:
         model = User
         fields = ['username', 'email', 'name', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'نام کاربری'}),
+            'email': forms.TextInput(attrs={'placeholder': 'ایمیل'}),
+            'name': forms.TextInput(attrs={'placeholder': 'نام و نام خانوادگی'}),
+            'password': forms.TextInput(attrs={'placeholder': 'رمز عبور'})
+        }
         error_messages = {
-            'username': {'required':'نام کاربری را وارد کنید'},
-            'email': {'required':'ایمیل را وارد کنید'
-                     ,'invalid':'ایمیل صحیح نمی باشد'},
-            'name': {'required': 'نامتان را وارد کنید'},
-            'password': {'required': 'پسورد را وارد کنید'}
+            'email': {'invalid':'ایمیل صحیح نمی باشد'}
         }
 
 
 class ForgotPass(forms.Form):
     email = forms.EmailField()
     captcha = CaptchaField()
+
+    email.widget.attrs['required']= 'required'
+    email.widget.attrs['placeholder']= 'ایمیل'
+
+    captcha.widget.attrs['required']= 'required'
+    captcha.widget.attrs['placeholder']= 'کد امنیتی'
+
+
